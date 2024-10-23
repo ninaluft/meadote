69c5fea86bfd045c14d2f67ffa694f7da3e48b8d
ninaluft <marinaluft@hotmail.com>
2024-10-23 10:36:14 -0300

    feat: add loading spinner to submit button and prevent multiple clicks

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,11 +1,9 @@
-<!-- resources/views/vendor/jetstream/components/button.blade.php -->
-
 @props([
     'icon' => null, // Ícone opcional
     'color' => 'gray', // Cor do botão, com padrão 'gray'
     'size' => 'md', // Tamanho do botão: 'sm', 'md', 'lg'
     'href' => null, // Link opcional para transformar o botão em um link
-    'ariaLabel' => '' // Acessibilidade
+    'ariaLabel' => '', // Acessibilidade
 ])
 
 @php
@@ -28,7 +26,7 @@
 
 @if($href)
     <a href="{{ $href }}"
-        {{ $attributes->merge(['class' => "inline-flex items-center $sizeClasses $colorClasses rounded-md   text-white  focus:outline-none   disabled:opacity-50 "]) }}
+        {{ $attributes->merge(['class' => "inline-flex items-center $sizeClasses $colorClasses rounded-md text-white focus:outline-none disabled:opacity-50"]) }}
         aria-label="{{ $ariaLabel }}">
         @if ($icon)
             <i class="{{ $icon }} mr-2" aria-hidden="true"></i>
@@ -36,11 +34,26 @@
         {{ $slot }}
     </a>
 @else
-    <button {{ $attributes->merge(['type' => 'submit', 'class' => "inline-flex items-center $sizeClasses $colorClasses rounded-md   text-white  focus:outline-none   disabled:opacity-50 "]) }}
-        aria-label="{{ $ariaLabel }}">
+    <button {{ $attributes->merge(['type' => 'submit', 'class' => "inline-flex items-center $sizeClasses $colorClasses rounded-md text-white focus:outline-none disabled:opacity-50"]) }}
+        aria-label="{{ $ariaLabel }}"
+        onclick="showSpinner(this)">
+        <span class="spinner mr-2" style="display:none;">
+            <svg class="animate-spin h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8H4z"></path>
+            </svg>
+        </span>
         @if ($icon)
             <i class="{{ $icon }} mr-2" aria-hidden="true"></i>
         @endif
         {{ $slot }}
     </button>
 @endif
+
+<script>
+    function showSpinner(button) {
+        // Exibe o spinner e desabilita visualmente o botão, mas não interfere na submissão
+        button.querySelector('.spinner').style.display = 'inline-block';
+        button.classList.add('opacity-50', 'cursor-not-allowed'); // Adiciona uma aparência desativada
+    }
+</script>
